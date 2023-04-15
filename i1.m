221deebbce60d4a7696a196c0bfe0ae75c5397d8
Junzhe Huang <richard_huang@Junzhes-MacBook-Pro.local>
2023-04-15 00:26:33 -0400

    small update

--- i1.m
+++ i1.m
@@ -1,8 +1,13 @@
 function [rt] = i1(I,x,y,F,theta,sigma)
 rt = 0;
-[M, N] = size(I);
+[M,N] = size(I);
 for tempX= x-2*sigma : x+2*sigma
-    rt = rt + double(I((tempX),y)) * h1(tempX,y,F,theta,sigma);
+    if x-tempX < 1
+        temp = M + x-tempX;
+    else
+        temp = x-tempX;
+    end
+    rt = rt + double(I(temp,y)) * h1(tempX,y,F,theta,sigma) * realmax;
 end
 
 end
--- m.m
+++
@@ -1,6 +0,0 @@
-function [rt] = m(i2,x,y)
-%M 此处显示有关此函数的摘要
-%   此处显示详细说明
-rt = abs(i2(x,y));
-end
-
